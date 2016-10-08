aef635d6f580a5d7adc04d3f37e47cac0bb7a261
Ziyan <jaraidaniel@gmail.com>
2016-10-08 11:39:59 +0200

    mako: reorganize vendor directory
    Change-Id: Ibcd569d31de9dc2b68c3e620e78922b5bb061ab3

--- mako/proprietary/Android.mk
+++ mako/proprietary/Android.mk
@@ -18,7 +18,7 @@ ifeq ($(TARGET_DEVICE),mako)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libacdbloader
-LOCAL_SRC_FILES := libacdbloader.so
+LOCAL_SRC_FILES := vendor/lib/libacdbloader.so
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib
